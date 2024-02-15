506a94ad40a5890e8217cc7f4661aeda4cc8161f
Jaemyung <p.jaemyung91@gmail.com>
2024-02-15 17:27:02 +0900

    REFACTOR: modify DML

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -3,4 +3,4 @@ INSERT INTO users (username, password, enabled) VALUES ('user', '12345', '1');
 INSERT INTO authorities(username, authority) VALUES ('admin', 'admin');
 INSERT INTO authorities(username, authority) VALUES ('user', 'write');
 
-INSERT INTO customer (email, pwd, `role`) VALUES ('admin@email.com', '12345', 'admin');
+INSERT INTO customer (email, pwd, `role`) VALUES ('admin@email.com', '$2a$12$.nwOhO69Nlj1sVf18GcavOcR/xOzqMiXKWsktOuLagsgPO31pRQLW', 'admin');
