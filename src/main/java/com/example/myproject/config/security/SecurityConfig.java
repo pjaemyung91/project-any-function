c92bb82a66682be38746339fbb4594b3c22527fb
Jaemyung <p.jaemyung91@gmail.com>
2024-02-16 17:49:51 +0900

    FEAT: Add cors config

--- src/main/java/com/example/myproject/config/security/SecurityConfig.java
+++ src/main/java/com/example/myproject/config/security/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.example.myproject.config.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
 import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -19,6 +24,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 //@EnableWebSecurity
 @Configuration
@@ -27,15 +35,16 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((requests) -> requests
-//            .requestMatchers(PathRequest.toH2Console()).permitAll()
-            .requestMatchers("/register").permitAll()
-            .requestMatchers("/", "/main", "/users").authenticated())
+        http.cors(withDefaults())
             .csrf((csrf) -> csrf.disable())
+            .authorizeHttpRequests((requests) -> requests
+//            .requestMatchers(PathRequest.toH2Console()).permitAll()
+                .requestMatchers("/register").permitAll()
+                .requestMatchers("/", "/main", "/users").authenticated())
 //            .csrf(csrf -> csrf.ignoringRequestMatchers(PathRequest.toH2Console()))
 //            .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
-            .formLogin(Customizer.withDefaults())
-            .httpBasic(Customizer.withDefaults());
+            .formLogin(withDefaults())
+            .httpBasic(withDefaults());
 
         /**
          *  Configuration to deny all the requests
@@ -86,13 +95,27 @@ public class SecurityConfig {
      * 스프링 시큐리티에게 비밀번호 암호화 관련 정보를 주는 것으로
      * 인증 시 어떠한 방법으로 비밀번호를 확인할 지 알려주는 것임
      */
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
 
     @Bean
     public BCryptPasswordEncoder bcryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
