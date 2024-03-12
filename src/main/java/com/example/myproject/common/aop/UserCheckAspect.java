4dde8c6039fb3e16627ce83cec1010dd8c38b9bf
Jaemyung <p.jaemyung91@gmail.com>
2024-03-12 11:05:05 +0900

    FEAT: Add custom annotation and inject user information into custom annotation

---
+++ src/main/java/com/example/myproject/common/aop/UserCheckAspect.java
@@ -0,0 +1,49 @@
+package com.example.myproject.common.aop;
+
+import com.example.myproject.common.annotation.LoginId;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Parameter;
+import javax.security.auth.login.Configuration.Parameters;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class UserCheckAspect {
+
+//    @Around("@annotation(com.example.myproject.common.annotation.LoginId)")
+    @Around("execution(* com.example.myproject.*.controller.*.*(..))")
+//    @Around("@annotation(LoginId)")
+    public Object proceed(ProceedingJoinPoint joinPoint) {
+
+        try {
+            Object[] args = joinPoint.getArgs();
+            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+            Parameter[] parameters = signature.getMethod().getParameters();
+
+            for(int i = 0; i < parameters.length; i++) {
+                log.info("is annotation = {}", (parameters[i].isAnnotationPresent(LoginId.class)));
+                if(parameters[i].isAnnotationPresent(LoginId.class)) {
+                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    args[i] = authentication.getPrincipal();
+                }
+            }
+
+            return joinPoint.proceed(args);
+
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/example/myproject/vuetest/controller/VueRestController.java
+++ src/main/java/com/example/myproject/vuetest/controller/VueRestController.java
@@ -1,11 +1,17 @@
 package com.example.myproject.vuetest.controller;
 
+import com.example.myproject.common.annotation.LoginId;
 import com.example.myproject.vuetest.dto.CreateExperienceDto;
 import com.example.myproject.vuetest.service.VueService;
+import java.lang.reflect.Type;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,9 +27,8 @@ public class VueRestController {
     private final VueService vueServiceImpl;
 
     @PostMapping("/experiences")
-    public ResponseEntity findAllExperience(@RequestBody CreateExperienceDto createExperienceDto) {
-        log.info("experiences = {}", createExperienceDto.toString());
-
+    public ResponseEntity findAllExperience(@RequestBody CreateExperienceDto createExperienceDto
+        , @LoginId String loginId, @AuthenticationPrincipal String user) {
 //        vueServiceImpl.createExperience(createExperienceDto);
         return ResponseEntity.ok("ok");
     }
